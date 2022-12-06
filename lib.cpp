d0d8e782b9f51973495f0f6178adc6d2f4c4645c
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-06 17:25:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+bool carattere (char &a) {
+    bool segnalibro = false;
+ if  ((a > 64) && (a < 91)){
+     a = a + 32;
+     segnalibro = true;
+ }else if ( (a > 96) && ( a < 122)){
+     a = a -32;
+     segnalibro = true;
 
+ } else {
+     segnalibro = false;
+ } return segnalibro;
+
+
+
+
+
+
+}
